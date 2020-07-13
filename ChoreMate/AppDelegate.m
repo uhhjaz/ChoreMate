8da72bbf4f01d84ff6504c1542f39224c7a4f06f
Jasdeep Gill <jazgill@fb.com>
2020-07-13 09:44:32 -0700

    initial setup

--- ChoreMate/AppDelegate.m
+++ ChoreMate/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    ParseClientConfiguration *configuration = [ParseClientConfiguration configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+      configuration.applicationId = @"myAppId";
+      configuration.server = @"https://jaz-choremate.herokuapp.com/parse";
+        
+    }];
+    [Parse initializeWithConfiguration:configuration];
     return YES;
 }
 
